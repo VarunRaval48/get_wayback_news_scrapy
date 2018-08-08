212f4c85280ab2a7685cc68315a7bf5dcc17c4e6
Varun Raval <vraval48@gmail.com>
2018-08-08 18:02:23 -0400

    added rotating proxy, downloader middleware, support for saving state

--- scrapy_news/pipelines.py
+++ scrapy_news/pipelines.py
@@ -12,27 +12,35 @@ class ScrapyNewsPipeline(object):
 
 
 class SavepagePipeline(object):
-  def __init__(self):
-    self.saved_pages = set()
+  # def __init__(self):
+  # self.saved_pages = set()
 
   def process_item(self, item, spider):
+    spider.state['saved_pages'] = spider.state.get('saved_pages', set())
 
     pub_date = item['pub_date']
     access_info = item['access_info']
     r_addr = item['addr']
     page = item['page']
+    page_type = item['page_type']
+    if page_type is None:
+      page_type = 'NOTYPE'
 
     # save article if appropriate
     # check for publication dateget_page_info
-    if pub_date < access_info.start_date or pub_date > access_info.end_date:
-      return
+    if pub_date is not None:
+      if pub_date < access_info.allowed_start_date or pub_date > access_info.allowed_end_date:
+        return
+    else:
+      pub_date = '????????'
 
     # remove string after ? in address (they indicate sections)
     # TODO check here whether things after ? change pages
     r_addr = r_addr.split("?")[0]
 
-    article_name = '{}_{}'.format(pub_date, r_addr)
+    article_name = '{}_{}_{}'.format(pub_date, page_type, r_addr)
 
-    if article_name not in self.saved_pages:
-      self.saved_pages.add(article_name)
-      access_info.save_article(page, str(pub_date), r_addr, article_name)
+    if article_name not in spider.state['saved_pages']:
+      spider.state['saved_pages'].add(article_name)
+      access_info.save_article(page, str(pub_date), r_addr,
+                               access_info.dir_name, article_name)
--- scrapy_news/settings.py
+++ scrapy_news/settings.py
@@ -9,7 +9,7 @@
 #     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'scrapy_news'
+BOT_NAME = 'google'
 
 SPIDER_MODULES = ['scrapy_news.spiders']
 NEWSPIDER_MODULE = 'scrapy_news.spiders'
@@ -54,6 +54,11 @@ COOKIES_ENABLED = False
 #DOWNLOADER_MIDDLEWARES = {
 #    'scrapy_news.middlewares.ScrapyNewsDownloaderMiddleware': 543,
 #}
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_news.middlewares.RotateUserAgentMiddleware': 110,
+    'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
+    'rotating_proxies.middlewares.BanDetectionMiddleware': 620
+}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
@@ -79,7 +84,7 @@ AUTOTHROTTLE_START_DELAY = 1
 AUTOTHROTTLE_MAX_DELAY = 120
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-AUTOTHROTTLE_TARGET_CONCURRENCY = 100
+AUTOTHROTTLE_TARGET_CONCURRENCY = 50
 # Enable showing throttling stats for every response received:
 AUTOTHROTTLE_DEBUG = False
 
@@ -97,3 +102,26 @@ DEPTH_LIMIT = 3
 DEPTH_PRIORITY = -1
 
 REACTOR_THREADPOOL_MAXSIZE = 20
+
+ROTATING_PROXY_PATH = './proxy_1.txt'
+
+user_agent_list = [
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+    "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+    "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+]
